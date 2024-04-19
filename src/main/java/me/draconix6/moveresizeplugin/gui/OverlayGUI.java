ec167c78249b4968baee522f0ef650c2870534d4
char <45241413+char3210@users.noreply.github.com>
2024-04-19 12:25:55 -0500

    custom overlay

--- src/main/java/me/draconix6/moveresizeplugin/gui/OverlayGUI.java
+++ src/main/java/me/draconix6/moveresizeplugin/gui/OverlayGUI.java
@@ -2,10 +2,16 @@ package me.draconix6.moveresizeplugin.gui;
 
 import com.sun.jna.Native;
 import com.sun.jna.platform.win32.WinDef;
+import xyz.duncanruns.julti.JultiOptions;
 import xyz.duncanruns.julti.win32.User32;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Objects;
 
 public class OverlayGUI extends JFrame {
@@ -29,9 +35,23 @@ public class OverlayGUI extends JFrame {
         this.hwnd = new WinDef.HWND(Native.getWindowPointer(this));
         User32.INSTANCE.BringWindowToTop(this.hwnd);
 
-        this.icon = new ImageIcon(Objects.requireNonNull(this.getClass().getResource("/overlay.png")));
+        this.icon = new ImageIcon(getImageUrl());
         this.label = new JLabel(this.icon);
 
         this.setVisible(false);
+    }
+
+    private URL getImageUrl() {
+        try {
+            Path path = JultiOptions.getJultiDir().resolve("eyesee_overlay.png");
+            File file = path.toFile();
+            if (!file.exists()) {
+                Files.copy(Objects.requireNonNull(this.getClass().getResourceAsStream("/overlay.png")), path);
+            }
+            if (file.isFile()) {
+                return file.toURI().toURL();
+            }
+        } catch (IOException ignored) {}
+        return Objects.requireNonNull(this.getClass().getResource("/overlay.png"));
     }
 }
