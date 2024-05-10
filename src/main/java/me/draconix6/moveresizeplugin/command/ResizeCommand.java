6a1e266c230de5f91abf758efcacfbd31dd492d2
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2024-05-10 19:34:12 -0300

    Generalize toggleResize behaviour

--- src/main/java/me/draconix6/moveresizeplugin/command/ResizeCommand.java
+++ src/main/java/me/draconix6/moveresizeplugin/command/ResizeCommand.java
@@ -1,19 +1,11 @@
 package me.draconix6.moveresizeplugin.command;
 
-import com.sun.jna.Pointer;
-import com.sun.jna.platform.win32.WinDef;
 import me.draconix6.moveresizeplugin.MoveResizePlugin;
-import xyz.duncanruns.julti.JultiOptions;
 import xyz.duncanruns.julti.cancelrequester.CancelRequester;
 import xyz.duncanruns.julti.command.Command;
 import xyz.duncanruns.julti.command.CommandFailedException;
-import xyz.duncanruns.julti.instance.GameOptions;
 import xyz.duncanruns.julti.instance.MinecraftInstance;
 import xyz.duncanruns.julti.management.InstanceManager;
-import xyz.duncanruns.julti.util.WindowStateUtil;
-import xyz.duncanruns.julti.win32.User32;
-
-import java.awt.*;
 
 public class ResizeCommand extends Command {
 
@@ -40,63 +32,19 @@ public class ResizeCommand extends Command {
 
     @Override
     public void run(String[] args, CancelRequester cancelRequester) {
-        MinecraftInstance activeInstance = InstanceManager.getInstanceManager().getSelectedInstance();
-        if (activeInstance == null) {
+        MinecraftInstance instance = InstanceManager.getInstanceManager().getSelectedInstance();
+        if (instance == null) {
             throw new CommandFailedException("Instance is not active, cannot resize - please add the command as a script and assign a hotkey to it as per the instructions on the GitHub.");
         }
 
-        JultiOptions options = JultiOptions.getJultiOptions();
-        WinDef.HWND mcHwnd = activeInstance.getHwnd();
-
-        boolean stretching = true;
-        boolean useMagnifier = args.length > 2 && args[2].equals("zoom");
-
-        Rectangle currentBounds = WindowStateUtil.getHwndRectangle(mcHwnd);
-        Rectangle boundsToSet = new Rectangle(options.windowPos[0], options.windowPos[1], Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-        if (currentBounds.width == boundsToSet.width && currentBounds.height == boundsToSet.height) {
-            boundsToSet.width = options.playingWindowSize[0];
-            boundsToSet.height = options.playingWindowSize[1];
-            stretching = false;
-        }
-        if (options.windowPosIsCenter) {
-            boundsToSet = WindowStateUtil.withTopLeftToCenter(boundsToSet);
+        if (args.length > 2 && !args[2].equals("zoom")) {
+            throw new CommandFailedException("Unknown argument given to resize command: " + args[2]);
         }
 
-        if (!stretching) {
-            if (MoveResizePlugin.wasFullscreen) { // && useMagnifier
-                GameOptions go = activeInstance.getGameOptions();
-                activeInstance.getKeyPresser().pressKey(go.fullscreenKey);
-            }
-            WindowStateUtil.setHwndStyle(mcHwnd, MoveResizePlugin.winStyle);
-        } else {
-            // if (useMagnifier) {
-                MoveResizePlugin.wasFullscreen = activeInstance.isFullscreen();
-                if (MoveResizePlugin.wasFullscreen) {
-                    activeInstance.ensureNotFullscreen();
-                }
-            // }
-            MoveResizePlugin.winStyle = WindowStateUtil.getHwndStyle(mcHwnd);
-            WindowStateUtil.setHwndBorderless(mcHwnd);
-        }
+        boolean useMagnifier = args.length > 2;
+        int width = Integer.parseInt(args[0]);
+        int height = Integer.parseInt(args[1]);
 
-        // credits to priffin/tallmacro
-        if (User32.INSTANCE.SetWindowPos(
-                mcHwnd,
-                new WinDef.HWND(new Pointer(0)),
-                boundsToSet.x,
-                boundsToSet.y,
-                boundsToSet.width,
-                boundsToSet.height,
-                new WinDef.UINT(0x0400))
-        ) {
-            if (args.length > 2 && args[2].equals("zoom")) {
-                if (!stretching) {
-//                    Julti.log(Level.INFO, "hi");
-                    MoveResizePlugin.getESGui().hideEyeSee();
-                }
-                else MoveResizePlugin.getESGui().showEyeSee(boundsToSet, activeInstance);
-            }
-            User32.INSTANCE.SetForegroundWindow(mcHwnd);
-        }
+        MoveResizePlugin.toggleResize(instance, width, height, useMagnifier);
     }
 }
