c5ea642e2a324d71f7335c366481d4620cee66e4
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2024-05-10 19:42:09 -0300

    Synchronize lua functions

--- src/main/java/me/draconix6/moveresizeplugin/lua/MoveResizeLuaLibrary.java
+++ src/main/java/me/draconix6/moveresizeplugin/lua/MoveResizeLuaLibrary.java
@@ -7,6 +7,7 @@ import me.draconix6.moveresizeplugin.MoveResizePlugin;
 import me.draconix6.moveresizeplugin.win32.User32Extra;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.Varargs;
+import xyz.duncanruns.julti.Julti;
 import xyz.duncanruns.julti.JultiOptions;
 import xyz.duncanruns.julti.cancelrequester.CancelRequester;
 import xyz.duncanruns.julti.instance.MinecraftInstance;
@@ -27,7 +28,9 @@ public class MoveResizeLuaLibrary extends LuaLibrary {
     }
 
     private static MinecraftInstance getInstanceFromInt(int instanceNum) {
-        return InstanceManager.getInstanceManager().getInstances().get(instanceNum - 1);
+        synchronized (Julti.getJulti()) {
+            return InstanceManager.getInstanceManager().getInstances().get(instanceNum - 1);
+        }
     }
 
     @LuaDocumentation(description = "Gets the current windows cursor speed.")
@@ -44,92 +47,118 @@ public class MoveResizeLuaLibrary extends LuaLibrary {
 
     @LuaDocumentation(description = "Hides the EyeSee window.")
     public void hideEyeSee(Integer showInstanceAfter) {
-        MoveResizePlugin.getESGui().hideEyeSee();
-        if (showInstanceAfter != null) {
-            User32.INSTANCE.SetForegroundWindow(getInstanceFromInt(showInstanceAfter).getHwnd());
+        synchronized (Julti.getJulti()) {
+            MoveResizePlugin.getESGui().hideEyeSee();
+            if (showInstanceAfter != null) {
+                User32.INSTANCE.SetForegroundWindow(getInstanceFromInt(showInstanceAfter).getHwnd());
+            }
         }
     }
 
     @LuaDocumentation(description = "Shows the EyeSee window.")
     public void showEyeSee(int instanceNum, int instanceWidth, int instanceHeight) {
-        MinecraftInstance instance = getInstanceFromInt(instanceNum);
-        JultiOptions options = JultiOptions.getJultiOptions();
-        Rectangle instanceBounds = new Rectangle(options.windowPos[0], options.windowPos[1], instanceWidth, instanceHeight);
-        if (options.windowPosIsCenter) {
-            instanceBounds = WindowStateUtil.withTopLeftToCenter(instanceBounds);
+        synchronized (Julti.getJulti()) {
+            MinecraftInstance instance = getInstanceFromInt(instanceNum);
+            JultiOptions options = JultiOptions.getJultiOptions();
+            Rectangle instanceBounds = new Rectangle(options.windowPos[0], options.windowPos[1], instanceWidth, instanceHeight);
+            if (options.windowPosIsCenter) {
+                instanceBounds = WindowStateUtil.withTopLeftToCenter(instanceBounds);
+            }
+            MoveResizePlugin.getESGui().showEyeSee(instanceBounds, instance);
+            User32.INSTANCE.SetForegroundWindow(instance.getHwnd());
         }
-        MoveResizePlugin.getESGui().showEyeSee(instanceBounds, instance);
-        User32.INSTANCE.SetForegroundWindow(instance.getHwnd());
     }
 
     @LuaDocumentation(description = "Returns true if the EyeSee window is currently being shown, otherwise false.")
     public boolean isEyeSeeShown() {
-        return MoveResizePlugin.getESGui().isShowing();
+        synchronized (Julti.getJulti()) {
+            return MoveResizePlugin.getESGui().isShowing();
+        }
     }
 
     @LuaDocumentation(description = "Gets the size of the specified instance.", returnTypes = {"number", "number"})
     public Varargs getSize(int instanceNum) {
-        Rectangle hwndRectangle = WindowStateUtil.getHwndRectangle(getInstanceFromInt(instanceNum).getHwnd());
-        return varargsOf(new LuaValue[]{valueOf(hwndRectangle.width), valueOf(hwndRectangle.height)});
+        synchronized (Julti.getJulti()) {
+            Rectangle hwndRectangle = WindowStateUtil.getHwndRectangle(getInstanceFromInt(instanceNum).getHwnd());
+            return varargsOf(new LuaValue[]{valueOf(hwndRectangle.width), valueOf(hwndRectangle.height)});
+        }
     }
 
     @LuaDocumentation(description = "Gets the playing size from Julti options.", returnTypes = {"number", "number"})
     public Varargs getPlayingSize() {
-        int[] playingWindowSize = JultiOptions.getJultiOptions().playingWindowSize;
-        return varargsOf(new LuaValue[]{valueOf(playingWindowSize[0]), valueOf(playingWindowSize[1])});
+        synchronized (Julti.getJulti()) {
+            int[] playingWindowSize = JultiOptions.getJultiOptions().playingWindowSize;
+            return varargsOf(new LuaValue[]{valueOf(playingWindowSize[0]), valueOf(playingWindowSize[1])});
+        }
     }
 
     @LuaDocumentation(description = "Resizes the specified instance to the specified width/height.\nThe window position or center window position will remain the same depending on the windowPosIsCenter option.")
     public void resizeInstance(int instanceNum, int width, int height) {
-        JultiOptions options = JultiOptions.getJultiOptions();
-        Rectangle boundsToSet = new Rectangle(options.windowPos[0], options.windowPos[1], width, height);
-        if (options.windowPosIsCenter) {
-            boundsToSet = WindowStateUtil.withTopLeftToCenter(boundsToSet);
+        synchronized (Julti.getJulti()) {
+            JultiOptions options = JultiOptions.getJultiOptions();
+            Rectangle boundsToSet = new Rectangle(options.windowPos[0], options.windowPos[1], width, height);
+            if (options.windowPosIsCenter) {
+                boundsToSet = WindowStateUtil.withTopLeftToCenter(boundsToSet);
+            }
+            // credits to priffin/tallmacro
+            User32.INSTANCE.SetWindowPos(
+                    getInstanceFromInt(instanceNum).getHwnd(),
+                    new WinDef.HWND(new Pointer(0)),
+                    boundsToSet.x,
+                    boundsToSet.y,
+                    boundsToSet.width,
+                    boundsToSet.height,
+                    new WinDef.UINT(0x0400));
         }
-        // credits to priffin/tallmacro
-        User32.INSTANCE.SetWindowPos(
-                getInstanceFromInt(instanceNum).getHwnd(),
-                new WinDef.HWND(new Pointer(0)),
-                boundsToSet.x,
-                boundsToSet.y,
-                boundsToSet.width,
-                boundsToSet.height,
-                new WinDef.UINT(0x0400));
     }
 
     @LuaDocumentation(description = "Gets the window style for the specified instance.")
     public int getInstanceWStyle(int instanceNum) {
-        return WindowStateUtil.getHwndStyle(getInstanceFromInt(instanceNum).getHwnd());
+        synchronized (Julti.getJulti()) {
+            return WindowStateUtil.getHwndStyle(getInstanceFromInt(instanceNum).getHwnd());
+        }
     }
 
     @LuaDocumentation(description = "Sets the window style for the specified instance.")
     public void setInstanceWStyle(int instanceNum, int wStyle) {
-        WindowStateUtil.setHwndStyle(getInstanceFromInt(instanceNum).getHwnd(), wStyle);
+        synchronized (Julti.getJulti()) {
+            WindowStateUtil.setHwndStyle(getInstanceFromInt(instanceNum).getHwnd(), wStyle);
+        }
     }
 
     @LuaDocumentation(description = "Sets the specified instance to borderless.")
     public void setBorderless(int instanceNum) {
-        WindowStateUtil.setHwndBorderless(getInstanceFromInt(instanceNum).getHwnd());
+        synchronized (Julti.getJulti()) {
+            WindowStateUtil.setHwndBorderless(getInstanceFromInt(instanceNum).getHwnd());
+        }
     }
 
     @LuaDocumentation(description = "Presses the fullscreen hotkey on the specified instance.")
     public void pressFullscreen(int instanceNum) {
-        MinecraftInstance instance = getInstanceFromInt(instanceNum);
-        instance.getKeyPresser().pressKey(instance.getGameOptions().fullscreenKey);
+        synchronized (Julti.getJulti()) {
+            MinecraftInstance instance = getInstanceFromInt(instanceNum);
+            instance.getKeyPresser().pressKey(instance.getGameOptions().fullscreenKey);
+        }
     }
 
     @LuaDocumentation(description = "Attempts to ensure an instance is not in fullscreen and wait until it unfullscreens.")
     public void ensureNotFullscreen(int instanceNum) {
-        getInstanceFromInt(instanceNum).ensureNotFullscreen();
+        synchronized (Julti.getJulti()) {
+            getInstanceFromInt(instanceNum).ensureNotFullscreen();
+        }
     }
 
     @LuaDocumentation(description = "Checks if the specified instance is in fullscreen.")
     public boolean isFullscreen(int instanceNum) {
-        return getInstanceFromInt(instanceNum).isFullscreen();
+        synchronized (Julti.getJulti()) {
+            return getInstanceFromInt(instanceNum).isFullscreen();
+        }
     }
 
     @LuaDocumentation(description = "Runs the toggle resize functionality. The return value will be true if the instance is resized to the specified value and false if the resize is reversed back to the playing window size.")
     public boolean toggleResize(int instanceNum, int width, int height, boolean useMagnifier) {
-        return MoveResizePlugin.toggleResize(getInstanceFromInt(instanceNum), width, height, useMagnifier);
+        synchronized (Julti.getJulti()) {
+            return MoveResizePlugin.toggleResize(getInstanceFromInt(instanceNum), width, height, useMagnifier);
+        }
     }
 }
